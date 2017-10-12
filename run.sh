1225bb265e9c75d34baf9b15c167b2eddad909d0
Ubuntu <thang@thanggpu.kxg3qnfci5iehlj0ssy0vzbsoc.xx.internal.cloudapp.net>
2017-10-12 22:44:54 +0000

    code to run

---
+++ run.sh
@@ -0,0 +1,5 @@
+python correct_text.py --train_path ../data/preprocessed_movie_lines_train.txt \
+                       --val_path ../data/preprocessed_movie_lines_val.txt \
+                       --config DefaultMovieDialogConfig \
+                       --data_reader_type MovieDialogReader \
+                       --model_path ../movie_dialog_model
