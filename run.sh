1c657325ca035852e7ca2f5e7acc86ee54668cb2
Ubuntu <thang@thanggpu.kxg3qnfci5iehlj0ssy0vzbsoc.xx.internal.cloudapp.net>
2017-10-12 22:57:29 +0000

    create model dir

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+mkdir ../movie_dialog_model
 python correct_text.py --train_path ../data/preprocessed_movie_lines_train.txt \
                        --val_path ../data/preprocessed_movie_lines_val.txt \
                        --config DefaultMovieDialogConfig \
