7e971fd2a67fd8a8689bc7d3bdc3a9f831271242
Thang Duong <thang@thanggpu.kxg3qnfci5iehlj0ssy0vzbsoc.xx.internal.cloudapp.net>
2017-10-13 17:58:28 +0000

    added script to run test

---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python correct_text.py --test_path ../data/preprocessed_movie_lines_test.txt --train_path ../data/preprocessed_movie_lines_train.txt --config DefaultMovieDialogConfig --data_reader_type MovieDialogReader --model_path ../movie_dialog_model --decode
